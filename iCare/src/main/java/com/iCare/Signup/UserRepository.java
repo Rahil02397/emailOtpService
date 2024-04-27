c62d542821761f2e0883d18351be2d157e650088
Rahil02397 <155514693+Rahil02397@users.noreply.github.com>
2024-04-27 22:45:58 +0530

    Delete iCare directory

--- iCare/src/main/java/com/iCare/Signup/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iCare.Signup;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUserName(String userName);
-}
